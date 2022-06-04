74975f9a70d41527df55c42d30df03ca7fb0952b
Anton Pechkin <antonpechkinq@gmail.com>
2022-06-04 15:49:36 +0300

    Feature. Add function to format quantity

--- app/utility/format_recipe.py
+++ app/utility/format_recipe.py
@@ -1,4 +1,12 @@
-def capitalize_first(text: str):
+def format_quantity(quantity: int | float) -> float | int:
+    try:
+        integer, fractional = str(quantity).split('.')
+    except:
+        return int(quantity)
+    return float(quantity) if float(fractional) > 0 else int(integer)
+
+
+def capitalize_first(text: str) -> str:
     return text[0].upper() + text[1:]
 
 
@@ -9,10 +17,11 @@ def format_recipe(recipe: dict):
            f'Nationality: {recipe["nationality"]}\n'
     ingredients = ''
     if recipe['ingredients']:
-        ingredients = '\n'.join([f'{counter+1}) '
-                                 f'{capitalize_first(ingredient["name"])} - {ingredient["quantity"]}'
+        ingredients = '\n'.join([f'{counter+1})'
+                                 f'{capitalize_first(ingredient["name"])}'
+                                 f' {" - "+str(format_quantity(ingredient["quantity"])) if ingredient["quantity"] else ""}'
                                  f' {ingredient["unit"] if ingredient["unit"] else ""}'
                                  for counter, ingredient in enumerate(recipe['ingredients'])])
         ingredients = f'You will need:\n{ingredients}'
 
-    return f'{title}\n{ingredients}'
+    return f'{title}\n{ingredients}\n\n{recipe["instruction"]}'
