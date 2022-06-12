cdf90672511bd77933fe458f3fab809528ae1790
Anton Pechkin <antonpechkinq@gmail.com>
2022-06-13 00:27:48 +0300

    Feature. Rework random recipe

--- app/handlers/random_recipe.py
+++ app/handlers/random_recipe.py
@@ -2,11 +2,11 @@ from app import dp, bd
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 
-from app.utility.send_photo import send_photo
+from app.data.state_worker import update_recipe_idx
+from app.handlers.general import send_recipes
 
 
 @dp.message_handler(lambda message: message.text == 'Random recipe', state='*')
 async def random_recipe(message: types.Message, state: FSMContext):
-    recipe, photo = bd.get_random_recipe()
-    await send_photo(message.chat['id'], photo)
-    await message.answer(recipe)
+    await update_recipe_idx(state, bd.get_random_recipe())
+    await send_recipes(message, state)
