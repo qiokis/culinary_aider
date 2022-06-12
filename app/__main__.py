24a04b7dd42edacf5d4ff29a063270a47ab79808
Anton Pechkin <antonpechkinq@gmail.com>
2022-06-12 21:16:16 +0300

    Fix. Delete dead code

--- app/__main__.py
+++ app/__main__.py
@@ -1,12 +1,9 @@
 import logging
 import asyncio
 
-from aiogram import executor
 from app import main
 
-# from app import dp
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
-    # executor.start_polling(dp, skip_updates=True)
